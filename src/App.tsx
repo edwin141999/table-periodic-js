fffdd06e716f5f1f8b1122f32723b42c2b91904e
edwin141999 <gendevilman@gmail.com>
2024-04-02 14:28:29 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-lg">Hola mundo</h1>
+      <h3 className="text-base">Table Periodic</h3>
+    </>
+  );
+}
+
+export default App;
