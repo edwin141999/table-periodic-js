e9811feb1b41ddfc8bfcf33289b6682b47d6a740
edwin141999 <gendevilman@gmail.com>
2024-04-03 18:31:24 -0600

    Add Navigation Links into App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { Link, Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  // TODO: Depende su fase, cambiarle el color
+  // HACER QUE PUEDA VER MAS DETALLADO LA INFORMACIÓN DE CADA ELEMENTO
+
   return (
     <>
-      <h1 className="text-lg">Hola mundo</h1>
-      <h3 className="text-base">Table Periodic</h3>
+      <nav className="flex flex-row justify-between my-2">
+        <ul>
+          <Link to={"/"}>Home</Link>
+        </ul>
+        <ul>
+          <li>Estados de la materia</li>
+        </ul>
+      </nav>
+      <Outlet />
     </>
   );
 }
