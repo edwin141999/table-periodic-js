a5caef4b602538f20d1c1054f6a9e5d794c318c1
edwin141999 <gendevilman@gmail.com>
2024-04-11 16:41:04 -0600

    add utils to categories

---
+++ src/services/utils/categories.ts
@@ -0,0 +1,46 @@
+export const CATEGORIES = [
+  {
+    category: "alkali metal",
+    color: "FFC692",
+  },
+  {
+    category: "alkaline earth metal",
+    color: "FFE092",
+  },
+  {
+    category: "post-transition metal",
+    color: "FFF992",
+  },
+  {
+    category: "transition metal",
+    color: "EDFF92",
+  },
+  {
+    category: "lanthanide",
+    color: "D2FF91",
+  },
+  {
+    category: "actinide",
+    color: "B8FF91",
+  },
+  {
+    category: "metalloid",
+    color: "91FF9E",
+  },
+  {
+    category: "diatomic nonmetal",
+    color: "AB91FF",
+  },
+  {
+    category: "Halogenos",
+    color: "F991FF",
+  },
+  {
+    category: "noble gas",
+    color: "91DFFF",
+  },
+  {
+    category: "polyatomic nonmetal",
+    color: "E2D9FF",
+  },
+];
